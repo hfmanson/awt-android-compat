2a5afa9cb4cf3985a9749b647074d99353c33ac1
Henri Manson <hfmanson@gmail.com>
2019-05-11 10:48:52 +0200

    applet setParameters

--- app/src/main/java/java/applet/Applet.java
+++ app/src/main/java/java/applet/Applet.java
@@ -1,22 +1,21 @@
 package java.applet;
 
 import java.awt.Panel;
+import java.util.Map;
 
 public class Applet extends Panel {
+
+    private Map<String, String> parameters;
 
     public void init() {
 
     }
 
+    public void setParameters(Map<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
     public String getParameter(String name) {
-        //TODO String getParameter(String name)
-        if ("positions".equals(name)) {
-            return "730161011122421040,A30191012150806162,A30191404101212252,A30191012120211242,A30191303110300131";
-        } else if ("nodes".equals(name)) {
-            return "2144013641245,4145850246941368,4145850246942568,51246750257941458,4136850347942467";
-        } else if ("titles".equals(name)) {
-            return "sample, sample, sample, sample, sample";
-        }
-        return null;
+        return parameters.get(name);
     }
 }
--- app/src/main/java/net/pbdavey/awt/demo/Demo.java
+++ app/src/main/java/net/pbdavey/awt/demo/Demo.java
@@ -3,12 +3,15 @@ package net.pbdavey.awt.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import java.awt.Button;
 import java.awt.event.MouseListener;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import awttests.ButtonGrid;
 
@@ -37,17 +40,16 @@ public class Demo extends Activity {
         return null;
     }
 
-    private View getJarPlanksView() {
+    private View getAppletView(String appletClassName, Map<String, String> parameters) {
         try {
-            //planks planks = new planks();
-            //planks.init();
-            //return planks1.getAndroidView();
-            Class planksClass = Class.forName("planks");
-            Object planks = planksClass.newInstance();
-            Method init = planksClass.getMethod("init");
-            init.invoke(planks);
-            Method getAndroidView = planksClass.getMethod("getAndroidView");
-            return (View) getAndroidView.invoke(planks);
+            Class appletClass = Class.forName(appletClassName);
+            Object applet = appletClass.newInstance();
+            Method setParameters = appletClass.getMethod("setParameters", Map.class);
+            setParameters.invoke(applet, parameters);
+            Method init = appletClass.getMethod("init");
+            init.invoke(applet);
+            Method getAndroidView = appletClass.getMethod("getAndroidView");
+            return (View) getAndroidView.invoke(applet);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -62,6 +64,43 @@ public class Demo extends Activity {
         return null;
     }
 
+    private View getPlanksSampleView() {
+        String appletClassName = "planks";
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("positions", "730161011122421040,A30191012150806162,A30191404101212252,A30191012120211242,A30191303110300131");
+        parameters.put("nodes", "2144013641245,4145850246941368,4145850246942568,51246750257941458,4136850347942467");
+        parameters.put("titles", "sample, sample, sample, sample, sample");
+        return getAppletView(appletClassName, parameters);
+    }
+
+    private View getPlanks3PuzzlesView() {
+        String appletClassName = "planks";
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("positions", "750163142401214144,A80295636402222427,B902A6657548680232");
+        parameters.put("nodes", "3125402453235313641245,51247832385025674136841246534579315842356,413484367940357612458931474367952348A51358941469");
+        parameters.put("titles", "Deep-End, Smash-Hit, Route-66");
+        return getAppletView(appletClassName, parameters);
+    }
+
+    private View getPlanksHexView() {
+        String appletClassName = "planks";
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("positions", "97028450514565023226666");
+        parameters.put("nodes", "31454135630372354247831463267");
+        parameters.put("titles", "Four-by-four");
+        return getAppletView(appletClassName, parameters);
+    }
+
+    private View getNoLeftTurnView() {
+        String appletClassName = "lrs2";
+        return getAppletView(appletClassName, null);
+    }
+
+    private View getOMazeView() {
+        String appletClassName = "OMaze";
+        return getAppletView(appletClassName, null);
+    }
+
     private View getAwtTestView() {
         ButtonGrid buttonGrid = new ButtonGrid();
         buttonGrid.init();
@@ -72,8 +111,14 @@ public class Demo extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //setContentView(R.layout.borderlayout);
         //setContentView(getAwtTestView());
-        setContentView(getJarPlanksView());
+        //View appletView = getPlanks3PuzzlesView();
+        //View appletView = getPlanksHexView();
+        View appletView = getNoLeftTurnView();
+        //View appletView = getOMazeView();
+        setContentView(appletView);
     }
 }
